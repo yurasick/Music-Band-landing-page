0de2c765ca6fa44f63e23ae5201ab203f703117e
Yurasick <yuriy.dublyanko@gmail.com>
2026-03-22 19:26:37 +0200

    fixed the code

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,26 @@ document.querySelectorAll(".menu-item").forEach((item) => {
     document.querySelector(".burger-checkbox").checked = false;
   });
 });
+
+// текст, який виводиться в poput в залежності від вибраної кнопки
+const buttons = document.querySelectorAll(".button-js");
+const popupContent = document.querySelector(".content");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const concertItem = this.closest(".concert_info");
+    const day = concertItem.querySelector(".concert-info__day").textContent;
+    const city = concertItem.querySelector(".concert-info__city").textContent;
+    popupContent.innerHTML = `
+  <p>
+    Ваше замовлення квитка на концерт гурту «Грим та Грім»
+  </p>
+  <p>
+    📅 ${day}
+  </p>
+  <p>
+    📍 ${city}
+  </p>
+`;
+  });
+});
